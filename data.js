1cbd961bef1c31399bfcc1cf619bd5cf0b6bb3f3
Violett123456 <133950494+Violett123456@users.noreply.github.com>
2023-05-21 20:55:47 +0300

    Upload

---
+++ data.js
@@ -0,0 +1,137 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0--",
+      "name": "Кухня-Гостиная",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.7088243595899693,
+          "pitch": 0.11657641809043717,
+          "rotation": 0,
+          "target": "1-"
+        },
+        {
+          "yaw": 1.3968372595126137,
+          "pitch": 0.18728012529042282,
+          "rotation": 0,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-",
+      "name": "Прихожая",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.08911522964760366,
+          "pitch": 0.05926202934367453,
+          "rotation": 0,
+          "target": "2-"
+        },
+        {
+          "yaw": -0.7347802213957522,
+          "pitch": 0.1338041597932751,
+          "rotation": 0,
+          "target": "0--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-",
+      "name": "Санузел",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.68228816425738,
+          "pitch": 0.21812001494173572,
+          "rotation": 0,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
